28812609c75b375881c669bd5c92dbd2e295aaa5
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-08-21 11:52:43 -0400

    write code for 3 ways to solve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 3) array method
+
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+// Array method cleaned up:
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+// 2) ES6 reduce helper:
+
+// function reverse(str) {
+//     return str.split('').reduce((reversed, char) => {
+//         return char + recersed;
+//     }, '');
+// }
+
+// 3) ES6 reduce helper with arrow function - smilififed:
+
+function reverse(str) {
+    return str.split('').reduce((reversed, char) => char + reversed, '')
+}
+
+
+// 1) ES6 for loop:
+
+// function reverse(str) {
+// let reversed = '';
+
+// for (let character of str) {
+//     reversed = character + reversed;
+// }
+// return reversed;
+
+// }
 
 module.exports = reverse;
